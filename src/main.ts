a5052ce20cb1ee9f1b20e7323f43ae833bd7a72e
zeohan.dxs <zeohan.dxs@gmail.com>
2022-12-14 19:07:48 +0800

    basic login view

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+// import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
+// app.use(ElementPlus, {
+//     locale: zhCn,
+// })
 
 app.mount('#app')
---
+++ src/views/LoginView.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="login-container">
+        <el-form class="login-form" :model="loginForm" label-width="60px" label-position="left">
+            <el-form-item label="帐号">
+                <el-input v-model="loginForm.account" name="account" type="text" tabindex="1" autocomplete="on" />
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="loginForm.password" name="password" type="password" tabindex="2"
+                    autocomplete="off" />
+            </el-form-item>
+
+            <el-button type="primary" style="width:100%; margin-bottom:30px;">
+                登录
+            </el-button>
+        </el-form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+const loginForm = reactive({
+    account: '',
+    password: '',
+})
+</script>
+
+<style lang="scss">
+.login-container {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    background-color: #2d3a4b;
+
+    .login-form {
+        position: relative;
+        width: 520px;
+        max-width: 100%;
+        padding: 200px 35px 0;
+        margin: 0 auto;
+        overflow: hidden;
+    }
+
+    .el-form-item__label {
+        color: #fff;
+    }
+}
+</style>
