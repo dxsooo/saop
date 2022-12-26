c4e79e47899b643b5151ef72edeb58192e70c721
zeohan.dxs <zeohan.dxs@gmail.com>
2022-12-26 23:10:11 +0800

    add router link

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
     <div class="menu">
         <el-scrollbar>
             <el-menu>
-                <el-menu-item index="user">用户管理</el-menu-item>
-                <el-menu-item index="supplier">供应商管理</el-menu-item>
-                <el-menu-item index="project">项目管理</el-menu-item>
-                <el-menu-item index="task">任务管理</el-menu-item>
+                <el-menu-item index="user">
+                    <RouterLink to="/user">
+                        <span>用户管理</span>
+                    </RouterLink>
+                </el-menu-item>
+                <el-menu-item index="supplier">
+                    <RouterLink to="/supplier">
+                        <span>供应商管理</span>
+                    </RouterLink>
+                </el-menu-item>
+                <el-menu-item index="project">
+                    <RouterLink to="/project">
+                        <span>项目管理</span>
+                    </RouterLink>
+                </el-menu-item>
+                <el-menu-item index="task">
+                    <RouterLink to="/task">
+                        <span>任务管理</span>
+                    </RouterLink>
+                </el-menu-item>
             </el-menu>
         </el-scrollbar>
     </div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      redirect: '/user',
       component: HomeView,
       children: [
         {
